71e03bb6eaeacd1fbc1174cd026b2265cf799e8d
Priyanka-1731 <89460533+Priyanka-1731@users.noreply.github.com>
2023-07-05 20:28:09 +0530

    Add files via upload

---
+++ module4-solution/script.js
@@ -0,0 +1,10 @@
+
+names = ["Aaron", "Zaarah", "Gokul", "John", "Priya", "ayaan", "Raahil", "kumar", "jacob", "Parker"]
+for (var i = 0; i < names.length; i++) {
+    if (names[i][0] === "j" || names[i][0] === "J") {
+        console.log("Goodbye " + names[i])
+    }
+    else {
+        console.log("Hello " + names[i])
+    }
+}
